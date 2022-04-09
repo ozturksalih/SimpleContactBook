ac660553043a181d5fe4a47016260f4fb3ffa220
ozturksalih <salihozturk020@gmail.com>
2022-04-09 12:05:35 +0200

    Add logic, views

---
+++ Logic/ContactsViewLogic.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using FinalProjectSalihOzturk.Entities;
+using FinalProjectSalihOzturk.Helpers;
+using System.Collections.ObjectModel;
+using System.Windows.Controls;
+using System.Windows.Input;
+using FinalProjectSalihOzturk.DataAccess;
+
+namespace FinalProjectSalihOzturk.Logic
+{
+    public class ContactsViewLogic :ObservableObject
+    {
+        private Contact _selectedContact;
+
+        private IContactDal _dataAccess;
+
+        private bool _isEditMode;
+        public ObservableCollection<Contact> Contacts { get; private set; }
+        public ICommand EditCommand { get; private set; }
+        public ICommand SaveCommand { get; private set; }
+        public ICommand UpdateCommand { get; private set; }
+        public ICommand AddCommand { get; private set; }
+        public ICommand DeleteCommand { get; private set; }
+
+        public ContactsViewLogic(IContactDal dataAccess)
+        {
+            this._dataAccess = dataAccess;
+
+            EditCommand = new RelayCommand(Edit, CanEdit);
+            SaveCommand = new RelayCommand(Save, IsEdit);
+            UpdateCommand = new RelayCommand(Update);
+            AddCommand = new RelayCommand(Add);
+            DeleteCommand = new RelayCommand(Delete, CanDelete);
+        }
+
+        public void GetContacts(IEnumerable<Contact> contacts)
+        {
+            Contacts = new ObservableCollection<Contact>(contacts);
+            OnPropertyChanged("Contacts");
+        }
+
+        private bool CanDelete()
+        {
+            return SelectedContact != null;
+        }
+
+        private void Delete()
+        {
+            Contacts.Remove(SelectedContact);
+            Save();
+        }
+
+        private void Add()
+        {
+            var newContact = new Contact
+            {
+                Name = "N/A",
+                Surname = "N/A",
+                Email = "N/A",
+                PhoneNumber = "N/A",
+                DateOfBirth = DateTime.Now
+
+            };
+            Contacts.Add(newContact);
+            SelectedContact = newContact;
+        }
+
+        private void Update()
+        {
+            _dataAccess.Save(Contacts);
+        }
+
+        private bool IsEdit()
+        {
+            return IsEditMode;
+        }
+
+        private void Save()
+        {
+            _dataAccess.Save(Contacts);
+            IsEditMode = false;
+            OnPropertyChanged("SelectedContact");
+
+        }
+
+        private bool CanEdit()
+        {
+            if (SelectedContact == null)
+                return false;
+            return !IsEditMode;
+        }
+
+        private void Edit()
+        {
+            IsEditMode = true;
+        }
+
+        public Contact SelectedContact
+        {
+            get { return _selectedContact; }
+            set { OnPropertyChanged(ref _selectedContact, value); }
+        }
+
+        
+
+        public bool IsEditMode
+        {
+            get { return _isEditMode; }
+            set
+            {
+                OnPropertyChanged(ref _isEditMode, value);
+                OnPropertyChanged("IsDisplayMode");
+            }
+        }
+        public bool IsDisplayMode
+        {
+            get { return !_isEditMode; }
+        }
+    }
+}
---
+++ Logic/MainView.cs
@@ -0,0 +1,31 @@
+﻿using FinalProjectSalihOzturk.DataAccess;
+using FinalProjectSalihOzturk.Helpers;
+
+namespace FinalProjectSalihOzturk.Logic
+{
+    public class MainView:ObservableObject
+    {
+        private object _currentView;
+        private BookViewLogic _bookViews;
+
+        public MainView()
+        {
+            var dataAccess = new JsonContactDal();
+            BookViewLogic = new BookViewLogic(dataAccess);
+            CurrentView = BookViewLogic;
+        }
+
+        public object CurrentView
+        {
+            get { return _currentView; }
+            set { OnPropertyChanged(ref _currentView, value); }
+        }
+
+        public BookViewLogic BookViewLogic
+        {
+            get { return _bookViews; }
+            set { OnPropertyChanged(ref _bookViews, value); }
+        }
+
+    }
+}
