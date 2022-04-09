249cd9f163bea191e90c9532a590a5886e2dd8a9
ozturksalih <salihozturk020@gmail.com>
2022-04-09 17:55:33 +0200

    refactor views, change startup event

--- DataAccess/JsonContactDal.cs
+++ DataAccess/JsonContactDal.cs
@@ -7,7 +7,7 @@ namespace FinalProjectSalihOzturk.DataAccess
 {
     public class JsonContactDal :IContactDal
     {
-        private readonly string _dataPath = "resource/contactdata.json";
+        private readonly string _dataPath = "resource/Data.json";
 
         public IEnumerable<Contact> GetContacts()
         {
